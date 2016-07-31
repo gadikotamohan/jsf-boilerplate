d5039610d2db0dfb6d93ced153a9c7fe0917d7e3
Gadikota Mohan Raja <mohan@devathon.com>
2016-07-31 20:48:34 +0530

    db connection pooling(using JNDI)

--- src/com/emailtracker/beans/User.java
+++ src/com/emailtracker/beans/User.java
@@ -1,22 +1,36 @@
 package com.emailtracker.beans;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 import org.primefaces.context.RequestContext;
 
 @ManagedBean(name = "user")
 public class User {
-	public User(){
-		
+	@Resource(name="jdbc/postgres")
+	private DataSource ds;
+	public User() throws NamingException{
+		Context ctx = new InitialContext();
+		ds = (DataSource)ctx.lookup("java:comp/env/jdbc/postgres");	  
 	}
+
     private String username;
     
     private String password;
  
-    public String getUsername() {
+    public String getUsername() throws SQLException{
         return username;
     }
  
@@ -32,12 +46,21 @@ public class User {
         this.password = password;
     }
    
-    public void login(ActionEvent event) {
+    public void login(ActionEvent event) throws SQLException{
         RequestContext context = RequestContext.getCurrentInstance();
         FacesMessage message = null;
         boolean loggedIn = false;
-         
-        if(username != null && username.equals("admin") && password != null && password.equals("admin")) {
+    	PreparedStatement ps = ds.getConnection().prepareStatement("select * from users WHERE username = ? AND password= ? LIMIT 1");
+    	ps.setString(1, username);
+    	ps.setString(2, password);
+    	ResultSet rs = ps.executeQuery();
+//    	while(rs.next()){
+//    		this.username = rs.getString("username");
+//    		System.err.println("Username --> "+rs.getString("username"));
+//    		System.err.println("Password --> "+rs.getString("password"));
+//    	}
+
+        if(rs.next()) {
             loggedIn = true;
             message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
         } else {
